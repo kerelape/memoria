ffba93a55e3ce2fc1fdad584d30e76c389ab98d8
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-04-18 01:16:06 +0500

    Add StringMemory

---
+++ core/src/main/kotlin/me/kerelape/memoria/core/BytesMemory.kt
@@ -0,0 +1,3 @@
+package me.kerelape.memoria.core
+
+interface BytesMemory : Memory<ByteArray>
---
+++ core/src/main/kotlin/me/kerelape/memoria/core/StringMemory.kt
@@ -0,0 +1,16 @@
+package me.kerelape.memoria.core
+
+import java.nio.charset.Charset
+
+class StringMemory(private val memory: BytesMemory, private val charset: Charset) : Memory<String> {
+
+    constructor(memory: BytesMemory) : this(memory, Charsets.UTF_8)
+
+    override fun write(value: String) {
+        this.memory.write(value.toByteArray(this.charset))
+    }
+
+    override fun value(): String {
+        return String(this.memory.value(), this.charset)
+    }
+}
