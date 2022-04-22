0a9a4fc747d917bbcee07cd7ef469cbb1016342c
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-04-22 15:35:08 +0500

    Add publish tasks

--- cactoos/build.gradle.kts
+++ cactoos/build.gradle.kts
@@ -1,7 +1,12 @@
 
 plugins {
     kotlin("jvm") version "1.6.20"
+    `java-library`
+    `maven-publish`
 }
+
+group = "me.kerelape.memoria"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -12,4 +17,29 @@ dependencies {
     implementation(module("org.cactoos:cactoos:0.49"))
 
     testImplementation(kotlin("test"))
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/kerelape/memoria")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_PASSWORD")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("GitHubPackages") {
+            groupId = project.group as String
+            artifactId = rootProject.name
+            version = project.version as String
+        }
+    }
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,9 +1,12 @@
 
 plugins {
     kotlin("jvm") version "1.6.20"
+    `maven-publish`
+    `java-library`
 }
 
 group = "me.kerelape.memoria"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -11,4 +14,29 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/kerelape/memoria")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_PASSWORD")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("GitHubPackages") {
+            groupId = project.group as String
+            artifactId = rootProject.name
+            version = project.version as String
+        }
+    }
 }
