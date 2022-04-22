40e013a1c332dbce932fed97239213cb2dc9f713
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-04-22 14:25:40 +0500

    Add explanation comment

--- cactoos/src/main/kotlin/me/kerelape/memoria/cactoos/OutputMemory.kt
+++ cactoos/src/main/kotlin/me/kerelape/memoria/cactoos/OutputMemory.kt
@@ -12,6 +12,9 @@ class OutputMemory(private val memory: BytesMemory) : Output {
             private val buffer: BytesMemory = RamMemory()
 
             override fun write(b: Int) {
+                // Check fails here due to '*' (spread) operator
+                // Tho I cannot see a more elegant way to do it
+                // @kerelape — issue #7
                 this.buffer.write(byteArrayOf(*this.buffer.value(), b.toByte()))
                 this@OutputMemory.memory.write(this.buffer.value())
             }
